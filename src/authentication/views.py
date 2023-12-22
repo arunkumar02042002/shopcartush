5edf2de64855462e21d71e2c1d93a84cce3c51a3
Arun Kumar <arun.kumar.2403gg@gmail.com>
2023-12-22 13:30:05 +0530

    Signup and Activate Account endpoint Done

---
+++ src/authentication/views.py
@@ -0,0 +1,131 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import CreateUserSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from common.helpers import validation_error_handler
+from authentication.models import User
+from .helpers import AuthHelper
+from django.conf import settings
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from .tokens import account_activation_token
+from common.utils import Utility
+import logging
+
+logger = logging.getLogger(__file__)
+
+# Create your views here.
+class SignUpView(APIView):
+
+    # DRF uses this variable to display the deafult html template
+    serializer_class = CreateUserSerializer
+
+    # Post request handler
+    def post(self, request, *args, **kwargs):
+        request_data = request.data
+        # data is required - otherwise it will not perform validations
+        serializer = self.serializer_class(data=request_data)
+
+        if serializer.is_valid() is False:
+            return Response({
+                "status": "error",
+                "message": validation_error_handler(serializer.errors), # For the toast
+                "payload": {
+                    "errors": serializer.errors
+                }
+            }, status.HTTP_400_BAD_REQUEST)
+        
+        validated_data = serializer.validated_data
+        email = validated_data['email']
+        password = validated_data['password']
+
+        # If verification fails because of third-party apps, user can again signup again
+        existing_user = User.objects.filter(email=email).first()
+
+        if existing_user is not None:
+            if existing_user.is_active is False:
+                existing_user.set_password(password)
+                existing_user.save()
+                user = existing_user
+            else:
+                return Response({
+                    "stautus": "error",
+                    "message": "Account with this email address already exists.",
+                    "payload": {},
+                }, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            username = AuthHelper.create_username(email=email)
+            user = User.objects.create_user(
+                username = username,
+                is_active = False,
+                **validated_data
+            )
+        
+        serializer_data = self.serializer_class(user).data
+
+        # send email
+        subject = "Verify Email for your Account Verification on WonderShop"
+        template = "auth/email/verify_email.html"
+        context_data = {
+            "host": settings.FRONTEND_HOST,
+            "uid": urlsafe_base64_encode(force_bytes(user.id)),
+            "token": account_activation_token.make_token(user=user),
+            "protocol": settings.FRONTEND_PROTOCOL
+        }
+
+        print(context_data)
+        try:
+            # Utility.send_mail_via_sendgrid(
+            #     user.email,
+            #     subject,
+            #     template,
+            #     context_data
+            # )
+            return Response({
+                "status": "success",
+                "message": "Sent the account verification link to your email address",
+                "payload": {
+                    **serializer_data,
+                    "tokens": AuthHelper.get_tokens_for_user(user) # For log in purpose, If the email is the verified this token will not work.
+                }
+            })
+        except Exception:
+            logger.error("Some error occurred in signup endpoint", exc_info=True)
+            return Response({
+                "status": "error",
+                "message": "Some error occurred",
+                "payload": {}
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        # send_verify_email_link.delay()
+        # return Response({
+        #     "status": "success",
+        #     "message": "Sent the account verification link to your email address",
+        #     "payload": {
+        #         **serializer_data,
+        #         "tokens": AuthHelper.get_tokens_for_user(user)
+        #     }
+        # })
+
+class ActivateAccountView(APIView):
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(id=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        if user is not None and account_activation_token.check_token(user, token):
+            user.is_active = True
+            user.save()
+
+            return Response({
+                'status': 'success',
+                'message': 'account verified',
+                'payload': {},
+            }, status=status.HTTP_200_OK)
+        return Response({
+            "status": "error",
+            "message": "Activation link is invalid",
+            "payload": {}
+        }, status=status.HTTP_403_FORBIDDEN)
