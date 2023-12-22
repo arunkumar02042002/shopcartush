b2a263bc90a3a41f68ba68a2b59823c5e048bc4e
Arun Kumar <arun.kumar.2403gg@gmail.com>
2023-12-22 15:12:37 +0530

    Added endpoint to refresh-token

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('activate/<uidb64>/<token>/', views.ActivateAccountView.as_view(), name='activate'),
-    path('login/', view=views.LoginView.as_view(), name='login')
+    path('login/', view=views.LoginView.as_view(), name='login'),
+    path('token/refresh/', view=views.TokenRefreshView.as_view(), name='refresh-token')
 ]
