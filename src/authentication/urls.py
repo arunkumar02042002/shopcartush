5fc8a8bf7e6256fc29129305e28e692d8dcff2db
Arun Kumar <arun.kumar.2403gg@gmail.com>
2023-12-22 14:13:22 +0530

    Added Login Endpoint

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
-    path('activate/<uidb64>/<token>/', views.ActivateAccountView.as_view(), name='activate')
+    path('activate/<uidb64>/<token>/', views.ActivateAccountView.as_view(), name='activate'),
+    path('login/', view=views.LoginView.as_view(), name='login')
 ]
