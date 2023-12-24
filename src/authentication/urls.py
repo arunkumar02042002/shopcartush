ec32eb1f7462b11970566f4c7acd83bdc73c79cc
Arun Kumar <arun.kumar.2403gg@gmail.com>
2023-12-24 15:41:31 +0530

    Added Logout endpoint

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('activate/<uidb64>/<token>/', views.ActivateAccountView.as_view(), name='activate'),
     path('login/', view=views.LoginView.as_view(), name='login'),
-    path('token/refresh/', view=views.TokenRefreshView.as_view(), name='refresh-token')
+    path('token/refresh/', view=views.TokenRefreshView.as_view(), name='refresh-token'),
+    path('logout/', view=views.UserLogoutView.as_view(), name='logout')
 ]
