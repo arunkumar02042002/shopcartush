5433c90cb117ca8323e0a954ddf046f021832c3d
Arun Kumar <arun.kumar.2403gg@gmail.com>
2023-12-26 11:02:46 +0530

    Model Register

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product, ProductTag, Tag
 
 # Register your models here.
+admin.site.register(ProductTag)
+admin.site.register(Product)
+admin.site.register(Tag)
