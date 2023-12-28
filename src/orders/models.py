e4dcc488c2d791ac649f60f011e25c23a913f96c
Arun Kumar <arun.kumar.2403gg@gmail.com>
2023-12-28 15:31:50 +0530

    Added new fields

--- src/orders/models.py
+++ src/orders/models.py
@@ -10,14 +10,15 @@ User = get_user_model()
 # Create your models here.
 class Order(TimeStampedModel):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    provider_order_id = models.CharField(max_length=255, blank=True, null=True)
     payment_id = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     status = models.CharField(choices=OrderStatus.CHOICES_LIST, max_length=16)
 
 
 class OrderItem(TimeStampedModel):
-    product_id = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveBigIntegerField()
