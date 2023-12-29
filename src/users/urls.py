b818c28b59a70b4ab76bccdeda865db1cb4e8c56
Arun Kumar <arun.kumar.2403gg@gmail.com>
2023-12-29 21:43:16 +0530

    updated

--- src/users/urls.py
+++ src/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addtocart/', views.AddToCartView.as_view(), name='add_to_cart'),
+    path('addtocart/', views.AddtoCartView.as_view(), name='add_to_cart'),
     path('cart/', view=views.DisplayCartView.as_view(), name='display_cart')
 ]
