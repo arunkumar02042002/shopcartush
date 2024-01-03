66eeaa563d66eaa8019d7fadb2db233ea4d81c09
Arun Kumar <arun.kumar.2403gg@gmail.com>
2024-01-03 16:23:44 +0530

    Added notifications endpoint

--- src/admin_dashboard/urls.py
+++ src/admin_dashboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path
 from products.views import UploadProductImageView, ListCreateProductView
+from notifications.views import ScheduleNotificationView
 
 urlpatterns = [
     path('products/', ListCreateProductView.as_view(), name='list-create-product'),
     path('products/image_upload/', UploadProductImageView.as_view(), name='product-image-upload'),
+    path('notifications/', ScheduleNotificationView.as_view(), name='schedule-notification')
     # re_path(r'^(?P<version>(v1|v2))/products/$', ListCreateProductView.as_view(), name='list-create-product'),
 ]
